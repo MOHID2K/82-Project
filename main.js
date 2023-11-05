882e8e2e88c32de9bb1d5de665a97e8996433735
MOHID2K <138736930+MOHID2K@users.noreply.github.com>
2023-11-05 08:31:22 -0500

    Add files via upload

---
+++ main.js
@@ -0,0 +1,27 @@
+console.log("My first console project");
+
+var first_name = "Ranbir";
+var last_name = "Kapoor";
+
+// Concatenate the first name and the last name to print the full name of the person on the console.
+var full_name = first_name.concat(last_name);
+console.log(full_name);
+
+var a = 10;
+var b = 5;
+
+// Create a variable named add and store the addition value of a and b in that variable.
+var add = a + b;
+console.log(add);
+
+// Create a variable named sub and store the subtraction value of a and b in that variable.
+var sub = a - b;
+console.log(sub);
+
+// Create a variable named multiple and store the multiplication value of a and b in that variable.
+var multiple = a * b;
+console.log(multiple);
+
+// Create a variable named divide and store the division value of a and b in that variable.
+var divide = a / b;
+console.log(divide);
